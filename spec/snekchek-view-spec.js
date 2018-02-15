16711868750232bcbff55049f05bbdd104bb2212
hargoniX <hargonix@gmail.com>
2018-02-15 16:31:13 +0100

    my code

---
+++ spec/snekchek-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SnekchekView from '../lib/snekchek-view';
+
+describe('SnekchekView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
